a753aae125d73b484fea9eef69227909640bd3c4
kensiiwasaki <kensiiwasaki@gmail.com>
2023-11-20 21:06:17 +0900

    favorite処理

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,10 +1,9 @@
 import type { FunctionComponent } from "react";
 import { json } from "@remix-run/node";
 import { Form, useLoaderData, useFetcher } from "@remix-run/react";
-import { getContact } from "../data";
-import type { LoaderFunctionArgs } from "@remix-run/node";
+import { getContact, updateContact } from "../data";
+import type { LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node";
 import invariant from "tiny-invariant";
-
 import type { ContactRecord } from "../data";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
@@ -14,6 +13,14 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
     throw new Response("Not Found", { status: 404 });
   }
   return json({ contact });
+};
+
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
 };
 
 export default function Contact() {
